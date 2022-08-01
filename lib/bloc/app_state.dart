fd4a9b1544d342bf5ef3bb04cf16617e72c41d1f
Fridho Kesuma Pratama <fridho.pratama@ctcorpdigital.com>
2022-08-01 13:12:56 +0700

    Finished bloc states

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,6 +1,95 @@
 part of 'app_bloc.dart';
 
 @immutable
-abstract class AppState {}
+abstract class AppState {
+  final bool isLoading;
+  final AuthError? authError;
 
-class AppInitial extends AppState {}
+  const AppState({
+    required this.isLoading,
+    this.authError,
+  });
+}
+
+class AppStateLoggedIn extends AppState {
+  final User user;
+  final Iterable<Reference> images;
+
+  const AppStateLoggedIn({
+    required this.user,
+    required this.images,
+    required bool isLoading,
+    AuthError? authError,
+  }) : super(
+          isLoading: isLoading,
+          authError: authError,
+        );
+
+  @override
+  bool operator ==(other) {
+    if (other is AppStateLoggedIn) {
+      return isLoading == other.isLoading &&
+          user.uid == other.user.uid &&
+          images.length == other.images.length;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        user.uid,
+        images,
+      );
+
+  @override
+  String toString() => 'AppStateLoggedIn, images.length = ${images.length}';
+}
+
+@immutable
+class AppStateLoggedOut extends AppState {
+  const AppStateLoggedOut({
+    required bool isLoading,
+    AuthError? authError,
+  }) : super(
+          isLoading: isLoading,
+          authError: authError,
+        );
+
+  @override
+  String toString() =>
+      'AppStateLoggedOut, isLoading = $isLoading, authError = $authError';
+}
+
+@immutable
+class AppStateIsInRegistration extends AppState {
+  const AppStateIsInRegistration({
+    required bool isLoading,
+    AuthError? authError,
+  }) : super(
+          isLoading: isLoading,
+          authError: authError,
+        );
+}
+
+extension GetUserId on AppState {
+  User? get user {
+    final cls = this;
+    if (cls is AppStateLoggedIn) {
+      return cls.user;
+    } else {
+      return null;
+    }
+  }
+}
+
+extension GetImages on AppState {
+  Iterable<Reference>? get images {
+    final cls = this;
+    if (cls is AppStateLoggedIn) {
+      return cls.images;
+    } else {
+      return null;
+    }
+  }
+}
